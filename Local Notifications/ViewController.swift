8e3e5dfc6bb10e35539307e1fdefa5ba91e23fe6
Prashant G <prashant.gaikwad@leftrightmind.com>
2018-10-05 12:52:26 +0530

    Showing local-notification on scheduled time

--- Local Notifications/ViewController.swift
+++ Local Notifications/ViewController.swift
@@ -19,11 +19,37 @@ class ViewController: UIViewController {
         content.body = "iOS developer"
         content.sound = UNNotificationSound.default()
         
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        //let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
         
-        let request = UNNotificationRequest(identifier: "myId", content: content, trigger: trigger)
+        //let request = UNNotificationRequest(identifier: "myId", content: content, trigger: trigger)
         
-        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+        //UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+        
+        // Configure the recurring date.
+        var dateComponents = DateComponents()
+        dateComponents.calendar = Calendar.current
+        
+        dateComponents.weekday = 6  // Tuesday
+        dateComponents.hour = 18    // 12:00 hours
+        dateComponents.minute = 00
+        
+        // Create the trigger as a repeating event.
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        
+        // Create the request
+        let uuidString = UUID().uuidString
+        let request = UNNotificationRequest(identifier: uuidString,
+                                            content: content, trigger: trigger)
+        
+        // Schedule the request with the system.
+        let notificationCenter = UNUserNotificationCenter.current()
+        notificationCenter.add(request) { (error) in
+            if error != nil {
+                // Handle any errors.
+                
+            }
+        }
+        
 
     }
 
